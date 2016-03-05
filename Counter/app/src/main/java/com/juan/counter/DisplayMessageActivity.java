80ff74f570f4f56f50f70d522e0c5cafc94450a5
Juan Liu <liujuan2010@gmail.com>
2016-03-04 23:38:22 -0600

    add display message activity

--- Counter/app/src/main/java/com/juan/counter/DisplayMessageActivity.java
+++ Counter/app/src/main/java/com/juan/counter/DisplayMessageActivity.java
@@ -1,15 +1,22 @@
 package com.juan.counter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+
     }
 
 }
