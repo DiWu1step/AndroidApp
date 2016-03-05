b295945943f6e929096e8b61e9267607bd779533
Juan Liu <liujuan2010@gmail.com>
2016-03-04 23:04:03 -0600

    workable version

---
+++ Counter/app/src/main/java/com/juan/counter/DisplayMessageActivity.java
@@ -0,0 +1,15 @@
+package com.juan.counter;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
--- Counter/app/src/main/java/com/juan/counter/MainActivity.java
+++ Counter/app/src/main/java/com/juan/counter/MainActivity.java
@@ -1,12 +1,16 @@
 package com.juan.counter;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.juan.counter.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +38,13 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
